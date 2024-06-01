f835b0dbae4e96cf24ad099fdd108fd498e9f458
Zeng Heng <zengheng4@huawei.com>
2024-06-01 14:38:28 +0800

    BACKPORT: OPENEULER: arm64: cpufeature: Both the major and the minor version numbers need to be checked
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I92AK4
    
    -----------------------------
    
    Both the major version number and the minor version numbers need to be
    checked to confirm whether the MPAM Extension is supported on the
    platform.
    
    This supports MPAM v0.1 version for cpufeature detection, check [1] for
    details.
    
    [1] https://developer.arm.com/documentation/ddi0595/2021-12/AArch64-
    Registers/ID-AA64PFR1-EL1--AArch64-Processor-Feature-Register-1?lang=en
    
    Fixes: 21771eaaf93a ("arm64: cpufeature: discover CPU support for MPAM")
    Signed-off-by: Zeng Heng <zengheng4@huawei.com>
    
    Link: https://gitee.com/openeuler/kernel/commit/407390d1ce57f037d6575f4ae6a73a9d355b2388
    [Kexy: Resolved minor conflict in arch/arm64/kernel/cpuinfo.c]
    Signed-off-by: Kexy Biscuit <kexybiscuit@aosc.io>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -1179,7 +1179,8 @@ void __init init_cpu_features(struct cpuinfo_arm64 *info)
 		cpacr_restore(cpacr);
 	}
 
-	if (id_aa64pfr0_mpam(info->reg_id_aa64pfr0))
+	if (id_aa64pfr0_mpam(info->reg_id_aa64pfr0) ||
+	    id_aa64pfr1_mpamfrac(info->reg_id_aa64pfr1))
 		init_cpu_ftr_reg(SYS_MPAMIDR_EL1, info->reg_mpamidr);
 
 	if (id_aa64pfr1_mte(info->reg_id_aa64pfr1))
@@ -1438,7 +1439,8 @@ void update_cpu_features(int cpu,
 		cpacr_restore(cpacr);
 	}
 
-	if (id_aa64pfr0_mpam(info->reg_id_aa64pfr0)) {
+	if (id_aa64pfr0_mpam(info->reg_id_aa64pfr0) ||
+	    id_aa64pfr1_mpamfrac(info->reg_id_aa64pfr1)) {
 		taint |= check_update_ftr_reg(SYS_MPAMIDR_EL1, cpu,
 					info->reg_mpamidr, boot->reg_mpamidr);
 	}
@@ -2412,7 +2414,11 @@ cpucap_panic_on_conflict(const struct arm64_cpu_capabilities *cap)
 static bool
 test_has_mpam(const struct arm64_cpu_capabilities *entry, int scope)
 {
-	if (!has_cpuid_feature(entry, scope))
+	u64 pfr0 = read_sanitised_ftr_reg(SYS_ID_AA64PFR0_EL1);
+	u64 pfr1 = read_sanitised_ftr_reg(SYS_ID_AA64PFR1_EL1);
+
+	if (!id_aa64pfr0_mpam(pfr0) &&
+	    !id_aa64pfr1_mpamfrac(pfr1))
 		return false;
 
 	/* Check firmware actually enabled MPAM on this cpu. */
@@ -2956,7 +2962,6 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.capability = ARM64_MPAM,
 		.matches = test_has_mpam,
 		.cpu_enable = cpu_enable_mpam,
-		ARM64_CPUID_FIELDS(ID_AA64PFR0_EL1, MPAM, 1)
 	},
 	{
 		.desc = "Memory Partitioning And Monitoring Virtualisation",
